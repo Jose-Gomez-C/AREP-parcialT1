abc984d4d6d60c38b9f9ae9a36e7dec0da49ab65
Jose Luis Gomez Camacho <josemania10@gmail.com>
2020-09-04 13:06:24 -0500

    Subida del main

--- src/main/java/edu/escuelaing/arep/client/Client.java
+++ src/main/java/edu/escuelaing/arep/client/Client.java
@@ -1,21 +1,34 @@
 package edu.escuelaing.arep.client;
 
 import java.io.BufferedReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class Client {
-    public static void main(String[] args) {
-		BufferedReader in = null;
-		List<Double> numeros = new ArrayList<Double>();
-		numeros.add(9.0);
-		numeros.add(8.5);
-		numeros.add(2.6);
-		numeros.add(18.1);
-		numeros.add(73.2);
-		numeros.add(25.54);
-		numeros.add(15.26);
-		String query =  numeros.toString().replace("[", "").replace("]", "").replace(" ", "");
-		//URL herokuApp = new URL();
+	public static void main(String[] args) throws IOException {
+		try {
+			URL herokuApp = new URL("https://salty-mesa-17164.herokuapp.com/calculadora");
+			HttpsURLConnection conexion = (HttpsURLConnection)  herokuApp.openConnection();
+			conexion.setRequestMethod("POST");
+			conexion.setDoOutput(true);
+			String inputString = "15.0,30.0,51.0,15.15,38.2";
+
+			OutputStream os = conexion.getOutputStream();
+			byte[] input = inputString.getBytes("utf-8");
+			os.write(input, 0, input.length);
+			BufferedReader br = new BufferedReader(new InputStreamReader(conexion.getInputStream(),"utf-8"));
+			StringBuilder response = new StringBuilder();
+			String responseString = null;
+			while ((responseString = br.readLine()) != null) {
+				response.append(responseString.trim());
+			}
+			System.out.println(responseString.toString());
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
